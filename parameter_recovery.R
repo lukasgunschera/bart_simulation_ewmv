dafe24223fb25439b7f9e4cbc341610e8ac62470
Lukas Gunschera <l.gunschera@outlook.com>
2024-04-17 07:22:32 +0100

    add inverse temperature parameter

--- parameter_recovery.R
+++ parameter_recovery.R
@@ -20,111 +20,104 @@
 
 library(rstan); library(hBayesDM); library(bayesplot); library(gtools)
 
-
 task_simulation <- function(n = 1,
                             phi = 1,
                             eta = 1,
                             rho = 1,
                             tau = 1,
-                            lambda = 1
-                            ){
+                            lambda = 1) {
 
-  simulated_task <- data.frame()
+  # initialise dataset for saving outcome data
+  sim_data <- data.frame()
 
   # Save simulation parameters
-  simulation_parameters <- data.frame("subj" = 1:n,
-                                      "phi" = phi,
-                                      "eta" = eta,
-                                      "rho" = rho,
-                                      "tau" = tau,
-                                      "lambda" = lambda)
+  simulation_parameters <- data.frame(
+    "subj" = 1:n,
+    "phi" = phi,
+    "eta" = eta,
+    "rho" = rho,
+    "tau" = tau,
+    "lambda" = lambda
+  )
 
-  # define order of explosion points (same across participants)
-  trial_order <- c(64, 105, 39, 96, 88, 21, 121, 10, 64, 32, 64, 101, 26, 34, 47, 121, 64, 95, 75, 13)
+  # initialise parameters
+  trials <- 20 # maximum number of trials
+  u_gain <- 1 # reward for each pump (default = 1)
+  u_stop <- 0 # stopping utility (default = 0)
+  pump_max <- 128 # number of max pumps in experiment
+  explosion_point <- c(
+    64, 105, 39, 96, 88, 21, 121, 10, 64, 32,
+    64, 101, 26, 34, 47, 121, 64, 95, 75, 13
+  ) # fixed explosion points
 
-  for(subj in 1:n){
+  ## FOR LOOPS ---------------------------------------------------------------------------------- ##
 
-    agent_dat <- matrix(NA, ncol = 9, nrow = 64)
-    colnames(agent_dat) <- c("subjID", "trial", "trial_type",
-                             "pumps", "explode", "SU", "UT")
-    # UT = transfer probability
-    # SU = subjective utility for pumping
+  ### Participants ------------------------------------------------------------------------------ ##
+  for (j in 1:n) {
+    pumps_success <- 0 # number of successful pumps
+    pumps_total <- 0 # number of total pumps
+    p_burst <- phi[j] # belief of burstpoint
 
-    for(t in 1:20){
-      trial_type <- trial_order[t]
+    # Create matrix per subject
+    data_subj <-  matrix(NA, ncol = 3, nrow = trials)
+    colnames(data_subj) <- c("subjID", "pumps", "explosion")
 
-      agent_dat[t, "subjID"] <- subj
-      agent_dat[t, "trial"] <- t
+    ### Trials ---------------------------------------------------------------------------------- ##
+    for (k in 1:trials[j]) {
+      # set and reset parameters with each trial
+      u_loss <- 0 # amount of accumulated reward that can be lost
+      pump_count <- 0
 
-      # create offer
-      agent_dat[t, "trial_type"] <- trial_type
+      ### Pumps --------------------------------------------------------------------------------- ##
+      for (l in 1:explosion_point[k]) {
+        u_loss <- l - 1 # potential loss, equivalent to accumulated rewards
+        u_pump <- (1 - p_burst) * u_gain - p_burst * lambda[j] * u_loss +
+          rho[j] * p_burst * (1 - p_burst) * (u_gain + lambda[j] * u_loss)^2
 
-      S_E <- kE[subj] * agent_dat[t, "effort_a"]^2
-      S_R <- kR[subj] * agent_dat[t, "amount_a"]
-      SV <- S_R - S_E
-      # Choice probability
-      prob_A <- 1 / (1 + exp(-1*((alpha[subj]) + SV)))
+        p_pump <- 1 / (1 + exp(-tau[j] * (0 - u_pump))) # softmax
 
+        # Sample with computed probability whether participants pumps or transfers
+        continue_pump <- sample(1:0, 1, prob = c(p_pump, 1 - p_pump))
 
-      # Sample choice
-      agent_dat[t, "SV"] <- SV
-      agent_dat[t, "choice"] <- sample(1:0, 1, prob = c(prob_A, 1-prob_A))
-
-      # Update for next staircase iteration
-      # Accept
-      if(agent_dat[t, "choice"] == 1){
-        # no updating
-        if(staircases[trial_type][[1]][1] == 4
-           & staircases[trial_type][[1]][2] == 1){
-          staircases[trial_type][[1]] <- staircases[trial_type][[1]]
-
-          # update reward (decrease)
-        } else if(staircases[trial_type][[1]][1] == 4){
-          staircases[trial_type][[1]][2] <- staircases[trial_type][[1]][2]-1
-
-          # update effort (increase)
-        } else if(staircases[trial_type][[1]][2] == 1){
-          staircases[trial_type][[1]][1] <- staircases[trial_type][[1]][1]+1
-
-          # random update choice (increase effort or decrease reward)
+        if (continue_pump == 0) { # break loop if participants stops pumping
+          break()
         } else {
-          random_update <- sample(1:2, 1)
-          if(random_update == 1){
-            staircases[trial_type][[1]][1] <- staircases[trial_type][[1]][1]+1
-          } else {
-            staircases[trial_type][[1]][2] <- staircases[trial_type][[1]][2]-1
-          }
+          pump_count <- pump_count + 1
         }
-        # Reject
-      } else if(agent_dat[t, "choice"] == 0){
-        # no updating
-        if(staircases[trial_type][[1]][1] == 1 & staircases[trial_type][[1]][2] == 4){
-          staircases[trial_type][[1]] <- staircases[trial_type][[1]]
 
-          # update reward (increase)
-        } else if(staircases[trial_type][[1]][1] == 1){
-          staircases[trial_type][[1]][2] <- staircases[trial_type][[1]][2]+1
+      } # END PUMP LOOP
 
-          # update effort (decrease)
-        } else if(staircases[trial_type][[1]][2] == 4){
-          staircases[trial_type][[1]][1] <- staircases[trial_type][[1]][1]-1
+      # set stopping point due to 1) stopping, or 2) reaching explosion point
+      pumps_trial <- pump_count
 
-          # random update choice (decrease effort or increase reward)
-        } else {
-          random_update <- sample(1:2, 1)
-          if(random_update == 1){
-            staircases[trial_type][[1]][1] <- staircases[trial_type][[1]][1]-1
-          } else {
-            staircases[trial_type][[1]][2] <- staircases[trial_type][[1]][2]+1
-          }
-        }
-      }
-    }
+      # calculate whether balloon exploded on trial
+      explode_trial <- ifelse(pumps_trial == explosion_point[k], 1, 0)
 
-    agent_dat <- as.data.frame(agent_dat)
-    simulated_task <- rbind(simulated_task, agent_dat)
-  }
+      pumps_total <- pumps_total + pumps_trial
+      pumps_success <- pumps_success + pumps_trial - explode_trial
 
-  return(list("simulation_parameters" = simulation_parameters,
-              "simulated_task" = simulated_task))
-}
+      # update prior belief of bursting probability
+      p_burst <- phi[j] + (1 - exp(-pumps_total * eta[j])) *
+          ((0.0 + pumps_total - pumps_success) / pumps_total - phi[j])
+
+      # add to matrix
+      data_subj[k, "subjID"] <- j
+      data_subj[k, "pumps"] <- pumps_trial
+      data_subj[k, "explosion"] <- explode_trial
+
+    } # END TRIAL LOOP
+
+    # Add participant data to bottom of dataframe
+    sim_data <- rbind(sim_data, as.data.frame(data_subj))
+
+  } # END PARTICIPANT LOOP
+
+  sim_data_list <- list("data" = sim_data, "parameters" = simulation_parameters)
+  return(sim_data_list)
+
+} # END FUNCTION
+
+task_simulation(n = 1, phi = .5, eta = .2, rho = .7, tau = .3, lambda = .5)
+
+sim_data_list$
+
